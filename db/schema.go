76ac3f166f13cc90bf58d96293bb35adf223b7aa
Phil Schumann <metaleap@users.noreply.github.com>
2023-10-10 11:32:08 +0200

    [autopush] «sillema sillema nika su»

--- db/schema.go
+++ db/schema.go
@@ -1,24 +1,29 @@
 package db
 
 import (
+	"slices"
+
 	. "yo/ctx"
-	"yo/str"
 )
 
 func ListTables(ctx *Ctx) (ret []string) {
 	rows, err := DB.QueryContext(ctx, `
-		SELECT table_schema, table_name FROM information_schema.tables
-			WHERE table_schema = 'public'
-			ORDER BY table_schema, table_name`)
+		SELECT table_name, column_name, column_default, is_nullable, data_type FROM information_schema.columns
+			WHERE table_name IN
+	  			(SELECT table_name FROM information_schema.tables
+			  		WHERE (table_type = 'BASE TABLE') AND (table_schema NOT IN ('pg_catalog', 'information_schema')))
+	  		ORDER BY table_name, ordinal_position
+	`)
 	if err != nil {
 		panic(err)
 	}
 	defer rows.Close()
 	for rows.Next() {
-		var foo = make([]string, 2)
-		rows.Scan(ptrs(foo)...)
-		println(str.Fmt("%#v", foo))
-		// ret = append(ret, s)
+		var cols = make([]string, 5)
+		rows.Scan(ptrs(cols)...)
+		if !slices.Contains(ret, cols[0]) {
+			ret = append(ret, cols[0])
+		}
 	}
 	if err = rows.Err(); err != nil {
 		panic(err)
