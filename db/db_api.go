2eafb64bc8b5b419fada44f4e173e9a1ddbf30fb
Phil Schumann <metaleap@users.noreply.github.com>
2023-10-12 10:59:28 +0200

    [autopush] «I was making donuts and now I'm on a bus!»

--- db/db_api.go
+++ db/db_api.go
@@ -28,8 +28,16 @@ func registerApiHandlers[T any](desc *structDesc) {
 	yoserve.API["__/db/"+type_name+"/count"] = yoserve.Method(apiCount[T])
 }
 
-type argId struct{ ID I64 }
 type retCount struct{ Count int64 }
+type argId struct{ ID I64 }
+type argQuery[T any] struct{ Query *ApiQueryExpr[T] }
+
+func (me *argQuery[T]) toDbQ() q.Query {
+	if me != nil && me.Query != nil {
+		return me.Query.toDbQ()
+	}
+	return nil
+}
 
 func apiGetById[T any](ctx *Ctx, args *argId, ret *T) any {
 	if it := Get[T](ctx, args.ID); it != nil {
@@ -39,11 +47,8 @@ func apiGetById[T any](ctx *Ctx, args *argId, ret *T) any {
 	return nil
 }
 
-func apiCount[T any](ctx *Ctx, args *ApiQueryExpr[T], ret *retCount) any {
-	if args != nil {
-		args.validate()
-	}
-	ret.Count = Count[T](ctx, args.Query(), 0, "", nil)
+func apiCount[T any](ctx *Ctx, args *argQuery[T], ret *retCount) any {
+	ret.Count = Count[T](ctx, args.toDbQ(), 0, "", nil)
 	return ret
 }
 
@@ -87,14 +92,14 @@ type ApiQueryExpr[T any] struct {
 	IN  []ApiQueryVal[T]
 }
 
-func (me *ApiQueryExpr[T]) validate() {
+func (me *ApiQueryExpr[T]) Validate() {
 	// binary operators
 	for name, bin_op := range map[string][]ApiQueryVal[T]{"EQ": me.EQ, "NEQ": me.NEQ, "LT": me.LT, "LE": me.LE, "GT": me.GT, "GE": me.GE} {
 		if (len(bin_op) != 0) && (len(bin_op) != 2) {
 			panic("expected 2 operands for operator '" + name + "'")
 		}
 		for i := range bin_op {
-			bin_op[i].validate()
+			bin_op[i].Validate()
 		}
 	}
 	// IN operator
@@ -102,34 +107,34 @@ func (me *ApiQueryExpr[T]) validate() {
 		panic("expected rhs operand for 'IN' operator")
 	}
 	for i := range me.IN {
-		me.IN[i].validate()
+		me.IN[i].Validate()
 	}
 	for i := range me.AND {
-		me.AND[i].validate()
+		me.AND[i].Validate()
 	}
 	for i := range me.OR {
-		me.OR[i].validate()
+		me.OR[i].Validate()
 	}
 	if me.NOT != nil {
-		me.NOT.validate()
+		me.NOT.Validate()
 	}
 }
 
-func (me *ApiQueryExpr[T]) Query() q.Query {
+func (me *ApiQueryExpr[T]) toDbQ() q.Query {
 	switch {
 	case len(me.AND) >= 2:
-		return q.AllTrue(sl.Map(me.AND, func(it ApiQueryExpr[T]) q.Query { return it.Query() })...)
+		return q.AllTrue(sl.Map(me.AND, func(it ApiQueryExpr[T]) q.Query { return it.toDbQ() })...)
 	case len(me.OR) >= 2:
-		return q.EitherOr(sl.Map(me.OR, func(it ApiQueryExpr[T]) q.Query { return it.Query() })...)
+		return q.EitherOr(sl.Map(me.OR, func(it ApiQueryExpr[T]) q.Query { return it.toDbQ() })...)
 	case me.NOT != nil:
-		return q.Not(me.NOT.Query())
+		return q.Not(me.NOT.toDbQ())
 	case len(me.IN) >= 2:
-		return q.In(me.IN[0].Val(), sl.Map(me.IN[1:], func(it ApiQueryVal[T]) any { return it.Val() }))
+		return q.In(me.IN[0].val(), sl.Map(me.IN[1:], func(it ApiQueryVal[T]) any { return it.val() }))
 	}
 	return nil
 }
 
-func (me *ApiQueryVal[T]) validate() {
+func (me *ApiQueryVal[T]) Validate() {
 	num_set := 0
 	if me.Fld != nil {
 		num_set++
@@ -151,7 +156,7 @@ func (me *ApiQueryVal[T]) validate() {
 	}
 }
 
-func (me *ApiQueryVal[T]) Val() any {
+func (me *ApiQueryVal[T]) val() any {
 	switch {
 	case me.Fld != nil:
 		return *me.Fld
