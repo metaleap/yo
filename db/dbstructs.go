01ca32a2b80c63f8ba64577316e992fc4be3164e
Phil Schumann <metaleap@users.noreply.github.com>
2023-10-18 10:22:38 +0200

    [autopush] «My mind is making ashtrays in Dayton ...»

--- db/dbstructs.go
+++ db/dbstructs.go
@@ -427,3 +427,7 @@ func (me *DateTime) UnmarshalJSON(data []byte) error {
 func (me *DateTime) MarshalJSON() ([]byte, error) {
 	return ((*time.Time)(me)).MarshalJSON()
 }
+
+func (me *Text) Do(f func(string) string) {
+	*me = (Text)(f((string)(*me)))
+}
--- util/util.go
+++ util/util.go
@@ -63,16 +63,24 @@ func Either[T comparable](try ...T) T {
 	return none
 }
 
-func ToIdent(s string) string {
+func ToIdent(s string) string { return ToIdentWith(s, '_') }
+
+func ToIdentWith(s string, replaceChar byte) string {
 	s = strings.TrimSpace(s)
 	var buf strings.Builder
 	buf.Grow(len(s))
+	next_up := false
 	for i := 0; i < len(s); i++ {
 		c := s[i]
-		if is_ident := (c >= '0' && c <= '9') || (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'); !is_ident {
-			buf.WriteByte('_')
-		} else {
+		if is_ident := (c >= '0' && c <= '9') || (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'); is_ident {
+			if next_up && (c >= 'a' && c <= 'z') {
+				c, next_up = c-32, false
+			}
 			buf.WriteByte(c)
+		} else if replaceChar != 0 {
+			buf.WriteByte(replaceChar)
+		} else {
+			next_up = true
 		}
 	}
 	return buf.String()
