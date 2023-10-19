063fef44ad7bbf1b23a041afc959af5b2afe2649
Phil Schumann <metaleap@users.noreply.github.com>
2023-10-19 18:52:11 +0200

    [autopush] «Mother Earth is not flat!»

--- srv/ˍdevmode.go
+++ srv/ˍdevmode.go
@@ -35,7 +35,7 @@ func viaHttp[TIn any, TOut any](methodPath string, ctx *Ctx, args *TIn, client *
 	}
 
 	req, err := http.NewRequestWithContext(ctx, "POST",
-		"http://localhost:"+str.FromInt(Cfg.YO_API_HTTP_PORT)+"/"+str.TrimL(methodPath, "/"),
+		"http://localhost:"+str.FromInt(Cfg.YO_API_HTTP_PORT)+"/"+str.TrimL(AppApiUrlPrefix+methodPath, "/"),
 		bytes.NewReader(json_raw))
 	if err != nil {
 		panic(err)
