3173b5444f1a3c173f996aeb13541f221faf5b29
Phil Schumann <metaleap@users.noreply.github.com>
2023-10-22 12:43:12 +0200

    [autopush] «To err is humor.»

--- ˍdevmode.go
+++ ˍdevmode.go
@@ -13,16 +13,22 @@ import (
 func init() {
 	ts2jsAppSideStaticDir = func() {
 		WalkDir(yosrv.StaticFilesDirNameApp, func(fsPath string, fsEntry fs.DirEntry) {
-			is_dir := fsEntry.IsDir()
+			if fsEntry.IsDir() {
+				return
+			}
 			switch {
-			case (!is_dir) && str.Ends(fsPath, ".js"):
+			case str.Ends(fsPath, ".js"):
 				ts_file_path := FilePathSwapExt(fsPath, ".js", ".ts")
 				if !IsFile(ts_file_path) {
-					DelFile(fsPath)
+					println("rm " + fsPath)
+					// DelFile(fsPath)
 				}
-			case (!is_dir) && str.Ends(fsPath, ".ts") && !str.Ends(fsPath, ".d.ts"):
-				// js_file_path := path[:len(path)-len(".ts")] + ".js"
-				// is_newer := (!IsFile(js_file_path))
+			case str.Ends(fsPath, ".ts") && !str.Ends(fsPath, ".d.ts"):
+				js_file_path := FilePathSwapExt(fsPath, ".ts", ".js")
+				if IsNewer(fsPath, js_file_path) {
+					println("ts2js " + fsPath)
+					// TsFile2JsFileViaEsbuild(fsPath)
+				}
 			}
 		})
 	}
