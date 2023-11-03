6c34bd749c8fab5779b28f098b8fb1d42d65f38b
Phil Schumann <metaleap@users.noreply.github.com>
2023-11-03 19:52:20 +0100

    fortune sez: «sillema sillema nika su»

--- util/datetime.go
+++ util/datetime.go
@@ -9,3 +9,7 @@ var DoAfter = time.AfterFunc
 func DtAtZeroSecsUtc(t time.Time) time.Time {
 	return time.Date(t.Year(), t.Month(), t.Day(), t.Hour(), t.Minute(), 0, 0, time.UTC)
 }
+
+func DtAtZeroNanosUtc(t time.Time) time.Time {
+	return time.Date(t.Year(), t.Month(), t.Day(), t.Hour(), t.Minute(), t.Second(), 0, time.UTC)
+}
