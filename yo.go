ff747ecaac1f5de325a60852b50bf6a0e16dff28
Phil Schumann <metaleap@users.noreply.github.com>
2023-11-04 11:48:47 +0100

    fortune sez: «Murphy was an optimist.»

--- yo.go
+++ yo.go
@@ -24,7 +24,7 @@ func init() {
 }
 
 func Init(staticFileDirApp fs.FS, staticFileDirYo fs.FS) (listenAndServe func()) {
-	time.Local = time.UTC // between above `init` and now, `time` might have its own `init`-time ideas about setting `time.Local`...
+	time.Local = time.UTC // repeat of init() above because who knows what happened in between (well, so far, we do. but still =)
 	yosrv.StaticFileDirApp, yosrv.StaticFileDirYo =
 		staticFileDirApp, staticFileDirYo
 
