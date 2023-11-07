7ad5e6a05be3ec79ef10ce621e81ab046c510ad4
Phil Schumann <metaleap@users.noreply.github.com>
2023-11-07 12:49:50 +0100

    err-notify #2

--- errs.go
+++ errs.go
@@ -6,6 +6,7 @@ import (
 	yoctx "yo/ctx"
 	yodb "yo/db"
 	yojson "yo/json"
+	. "yo/util"
 	"yo/util/sl"
 	"yo/util/str"
 )
@@ -23,6 +24,8 @@ type ErrEntry struct {
 	HttpUrlPath   yodb.Text
 	HttpFullUri   yodb.Text
 	NumCaught     yodb.U8
+	JobRunId      yodb.I64
+	JobTaskId     yodb.I64
 }
 
 func init() {
@@ -32,11 +35,19 @@ func init() {
 		defer ctx.OnDone(nil)
 		ctx.DbTx()
 
+		var json_ctx_vals []byte
+		Try(func() {
+			json_ctx_vals = yojson.From(ctxVals, false)
+		}, nil)
+
 		err_entry := ErrEntry{
 			Err:           yodb.Text(str.FmtV(err)),
 			ErrDbRollback: yodb.Text(str.FmtV(errDbRollback)),
 			NumCaught:     1,
-			CtxVals:       yodb.Text(yojson.From(ctxVals, false)),
+			CtxVals:       yodb.Text(json_ctx_vals),
+		}
+		if nowInvalidCtx.Job != nil {
+			err_entry.JobRunId, err_entry.JobTaskId = yodb.I64(nowInvalidCtx.Job.RunId), yodb.I64(nowInvalidCtx.Job.TaskId)
 		}
 		if nowInvalidCtx.Http != nil {
 			err_entry.HttpUrlPath = yodb.Text(nowInvalidCtx.Http.UrlPath)
@@ -44,13 +55,24 @@ func init() {
 				err_entry.HttpFullUri = yodb.Text(nowInvalidCtx.Http.Req.RequestURI)
 			}
 		}
+
 		similar_enough := yodb.FindOne[ErrEntry](ctx,
-			ErrEntryErr.Equal(err_entry.Err).
-				And(ErrEntryErrDbRollback.Equal(err_entry.ErrDbRollback)).
-				And(ErrEntryHttpUrlPath.Equal(err_entry.HttpUrlPath)),
+			ErrEntryErr.Equal(err_entry.Err).And(ErrEntryErrDbRollback.Equal(err_entry.ErrDbRollback)),
 		)
 		if similar_enough == nil {
 			yodb.CreateOne[ErrEntry](ctx, &err_entry)
+		} else if similar_enough.NumCaught < 255 {
+			similar_enough.NumCaught++
+			similar_enough.CtxVals = If(len(ctxVals) == 0, similar_enough.CtxVals, err_entry.CtxVals)
+			similar_enough.JobRunId = If(err_entry.JobRunId == 0, similar_enough.JobRunId, err_entry.JobRunId)
+			if similar_enough.JobRunId == err_entry.JobRunId {
+				similar_enough.JobTaskId = If(err_entry.JobTaskId == 0, similar_enough.JobTaskId, err_entry.JobTaskId)
+			}
+			similar_enough.HttpFullUri = If(err_entry.HttpFullUri == "", similar_enough.HttpFullUri, err_entry.HttpFullUri)
+			similar_enough.HttpUrlPath = If(err_entry.HttpUrlPath == "", similar_enough.HttpUrlPath, err_entry.HttpUrlPath)
+			similar_enough.Err = If(err_entry.Err == "", similar_enough.Err, err_entry.Err)
+			similar_enough.ErrDbRollback = If(err_entry.ErrDbRollback == "", similar_enough.ErrDbRollback, err_entry.ErrDbRollback)
+			yodb.Update[ErrEntry](ctx, similar_enough, nil, false)
 		}
 	}
 }
