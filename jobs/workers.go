260a0ca1b72f9974fc5bb48d2ed38f3a1c0402f3
Phil Schumann <metaleap@users.noreply.github.com>
2023-11-10 14:24:59 +0100

    insert-via-unnest #3

--- jobs/workers.go
+++ jobs/workers.go
@@ -178,10 +178,7 @@ func (me *engine) startDueJob(ctxForCacheReuse *Ctx, jobRun *JobRun, jobDef *Job
 			return task
 		})
 		num_tasks += len(tasks)
-		println("MKTASKS:"+jobDef.Name+"...", len(tasks))
-		ctx.Db.PrintRawSqlInDevMode = true
 		yodb.CreateMany[JobTask](ctx, tasks...)
-		ctx.Db.PrintRawSqlInDevMode = false
 	})
 	done = true
 
