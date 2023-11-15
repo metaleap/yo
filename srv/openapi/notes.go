8282d4b9a5690db4e68596122d0be365b722d4d0
Phil Schumann <metaleap@users.noreply.github.com>
2023-11-15 13:27:55 +0100

    openapi

--- srv/openapi/notes.go
+++ srv/openapi/notes.go
@@ -14,8 +14,9 @@ ___
 Our backend stack's "opinionated convention-over-configuration" designs yield a few request/response rules that predictably remain **always in effect across all listed operations**:
 - Whereas request and response bodies are operation-specific, all operations share the exact-same set of request headers, URL query-string parameters and response headers (albeit being elaborated here identically and redundantly for each individual operation).
 - The empty request body is principally the JSON ´{}´, but fully-empty or JSON ´null´ request bodies are interpreted equivalently.
-- Response bodies will never be empty, but may be the JSON ´null´.
+  - Response bodies are never empty and are never the JSON ´null´.
 - Request and response bodies are always valid JSON values for _JSON objects_, ie. they're never immediately JSON arrays, ´string´s, ´number´s, or ´boolean´s.
+  - They're also never immediately "domain objects" (ie. a ´GetFoo´ op would yield not a ´Foo´ but a ´Result´ (or similar) field with the found ´Foo´; or a ´CreateFoo´ would expect not a ´Foo´ payload but a ´NewFoo´ (or similar) field with the new ´Foo´).
 - All mentioned request-object (and sub-object) fields are **by default optional** and ommittable or ´null´able (implying for atomic types semantic equivalence to ´""´ or ´0´ or ´false´ as per _Golang_ type-system semantics),
   - **any exceptions** to this optionality-by-default are indicated by the operation's listed known-error responses.
 - All mentioned response-object (and sub-object) fields will always be present in the response-body, indicating their default-ness / missing-ness via ´null´ or ´""´ or ´0´ or ´false´ as per _Golang_ type-system semantics.
@@ -48,7 +49,7 @@ About **error responses**:
 - **"Not Found" rules:**
   - ´404´ **only** for HTTP requests with definitely-unroutable URL paths (ie. "no such API operation or static-file asset or sub-site or etc."),
   - ´406´ with approx. ´FooDoesNotExist´ (see per-operation known-error-responses listings for exact monikers) — for operations where existence was definitely critically expected (such as modifying some object identified by its ´Id´),
-  - ´200´ with response-body of usually JSON ´null´ (exception: an operation-specific response object with a field name obviously indicating not-found-ness) — for operations where the definite-existence expectation does not hold as crucially (for example those of the "fetch single/first object found for some specified criteria" kind).
+  - ´200´ with some operation-specific response-body field (like ´Result´ or similar) being ´null´ — for operations where the definite-existence expectation does not hold as crucially (for example those of the "fetch single/first object found for some specified criteria" kind).
 
 `), str.Dict{"´": "`"})
 
